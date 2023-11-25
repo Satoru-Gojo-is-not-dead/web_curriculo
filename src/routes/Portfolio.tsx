92f794f34bc74e0127e72759f37b3404d7abb977
Nikolas Menezes <nikolasdev.contato@gmail.com>
2023-11-24 23:30:33 -0300

    configuração papapa

--- src/routes/Portfolio.tsx
+++ src/routes/Portfolio.tsx
@@ -1,4 +1,20 @@
+import { urlApi } from "../constants/api"
+
 const Portfolio = () => {
+
+  const getQualifications = async () => {
+    const response = await fetch(urlApi + "/api/get_qualifications.php", {
+      headers: {
+        'Content-type': "application/json",
+      },
+    })
+
+    const data = await response.json()
+
+    console.log(data)
+  }
+  getQualifications()
+
   return (
     <div>Portfolio</div>
   )
