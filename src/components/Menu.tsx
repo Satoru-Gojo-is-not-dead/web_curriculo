443462fd6777e682893dc9e0f3d7c2c42bd0341b
Nikolas Menezes <nikolasdev.contato@gmail.com>
2024-01-21 19:04:01 -0300

    correções layout

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom'
 import { useRef } from 'react'
 import '../styles/components/menu.sass'
-import { Link as ScrollLink } from 'react-scroll';
 
 const Menu = () => {
 
